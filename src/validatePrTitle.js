2cda6a4eebd30c7897c39b5260d647c14175fc8e
donotlb <donotlb@gmail.com>
2021-12-20 23:12:09 +0800

    fix: fix module not found error

--- src/validatePrTitle.js
+++ src/validatePrTitle.js
@@ -1,14 +1,8 @@
 const config = require('@commitlint/config-conventional');
 const lint = require('@commitlint/lint').default;
-const load = require('@commitlint/load').default;
 
 module.exports = async function validatePrTitle(prTitle) {
-  const opts = await load(config);
-  const result = await lint(
-    prTitle,
-    opts.rules,
-    opts.parserPreset ? {parserOpts: opts.parserPreset.parserOpts} : {}
-  );
+  const result = await lint(prTitle, config.rules);
 
   if (!result.valid) {
     const errorMessages = result.errors.map((error) => error.message);
